2ca1ca2afb8a84ccf7ae9ba0b805dabaf4157e54
SimYeonJae <112994031+SimYeonJae@users.noreply.github.com>
2022-11-10 01:17:16 +0900

    another
    another

--- 8thWeek/Pp.353_NO.도전실전예제/Pp.353_NO.도전실전예제/소스.cpp
+++ 8thWeek/Pp.353_NO.도전실전예제/Pp.353_NO.도전실전예제/소스.cpp
@@ -3,14 +3,14 @@
 
 int main(void)
 {
-	char str1[30] = {};
-	char str2[30] = {};
-	char str3[30] = {};
+    char str1[30] = {};
+    char str2[30] = {};
+    char str3[30] = {};
     char temp[30];
 
-	printf("세 단어 입력 : ");
-	scanf_s("%s%s%s", str1, 30, str2, 30, str3, 30);
-	printf("\n\nwords entered : %s %s %s", str1, str2, str3);
+    printf("세 단어 입력 : ");
+    scanf_s("%s%s%s", str1, 30, str2, 30, str3, 30);
+    printf("\n\n : %s %s %s", str1, str2, str3);
 
 
     if (strcmp(str1, str2) > 0)
@@ -33,6 +33,6 @@ int main(void)
         strcpy_s(temp, str2);
         strcpy_s(str2, str3);
         strcpy_s(str3, temp);
-	}
-	printf("\n\nAfter the change : %s %s %s\n\n", str1, str2, str3);
+    }
+    printf("\n\n  : %s %s %s\n\n", str1, str2, str3);
 }
--- 8thWeek/Pp.380_NO.도전실전예제/Pp.380_NO.도전실전예제/소스.cpp
+++ 8thWeek/Pp.380_NO.도전실전예제/Pp.380_NO.도전실전예제/소스.cpp
@@ -1,35 +1,36 @@
 #include <stdio.h>
 
-void input_data(int* a, int* b);
-void swap_data(int* a, int* b);
+void input_data(int* pa, int* pb);
+void swap_data(int* pa, int* pb);
 void print_data(int a, int b);
 
 int a, b;
 
 int main(void)
 {
-	input_data(&a, &b);
-	swap_data(&a, &b);
-	print_data(a, b);
+    input_data(&a, &b);
+    swap_data(&a, &b);
+    print_data(a, b);
 
-	return 0;
+    return 0;
 }
 
-void input_data(int* a, int* b)
+void input_data(int* pa, int* pb)
 {
-	printf("두 정수 입력 :");
-	scanf_s("%d%d", a, b);
+    printf(" 두 정수 입력 : ");
+    scanf_s("%d%d", pa, pb);
 }
 
-void swap_data(int* a, int* b)
+void swap_data(int* pa, int* pb)
 {
-	int temp = *a;
+    int temp;
 
-	*a = *b;
-	*b = temp;
+    temp = a;
+    a = b;
+    b = temp;
 }
 
 void print_data(int a, int b)
 {
-	printf("두 정수 출력 : %d, %d", a, b);
+    printf("> 두 정수 출력 : %d, %d\n", a, b);
 }
